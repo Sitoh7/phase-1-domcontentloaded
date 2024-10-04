80564db81b07fd0a782d8e1d005f7c22a40c933c
Sitoh7 <ha.wakhule@gmail.com>
2024-10-04 18:06:31 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded',generateText)
+
+
+function generateText(){
+    document.getElementById("text").textContent = `This is really cool!`
+}
